3fe64328eb74acceeb27445603f297d9339a2d28
ngenovictor <ngenovictor321@gmail.com>
2017-04-04 11:03:41 +0300

    Prime Number function version 1.0

--- day_two/prime_numbers.py
+++ day_two/prime_numbers.py
@@ -1,3 +1,17 @@
-# A working function to generate prime numbers from 0 to n with asymptotic analysis in your publice Github Repo
-def prime_number_generator():
-	pass
+# A working function to generate prime numbers from 0 to n 
+# with asymptotic analysis in your public Github Repo
+def prime_number_generator(n):
+	if type(n) != int:
+		return "You should only provide integer values"
+	elif n<0:
+		return "Only provide positive numbers"
+	elif n<2:
+		return "Use a value greater than 2"
+	else:
+		for num in range(2,n):
+			if num>1:
+				for x in range(2,num):
+					if (num%x==0):
+						break
+				else:	
+					print (num)
--- day_two/test_prime_numbers.py
+++ day_two/test_prime_numbers.py
@@ -2,8 +2,13 @@ import unittest
 from prime_numbers import prime_number_generator
 
 class PrimeNumberGenerator(unittest.TestCase):
-	def test_if_function_exists(self):
-		self.assertEqual(prime_number_generator(),None,"Error")
+	def test_if_value_given_is_integer(self):
+		self.assertEqual(prime_number_generator(0.5),"You should only provide integer values","Non-integer value provided")
+	def test_if_value_is_less_than_2(self):
+		self.assertEqual(prime_number_generator(0),"Use a value greater than 2","Value given less than 2")
+		self.assertEqual(prime_number_generator(1),"Use a value greater than 2","Value given less than 2")
+	def test_if_value_is_positive(self):
+		self.assertEqual(prime_number_generator(-1),"Only provide positive numbers","Value given is negative")
 
 if __name__ == "__main__":
 	unittest.main()
